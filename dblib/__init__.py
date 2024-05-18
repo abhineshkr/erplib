079b84b746c0c7d6ec5dbb4ee0731c16b12c9b3d
abhineshkr <abhineshkr1987@gmail.com>
2024-05-18 18:50:34 +0530

    update

--- dblib/__init__.py
+++ dblib/__init__.py
@@ -2,4 +2,5 @@
 
 from .mymodule import hello_world
 from .mymodule import hello_world2
+from .db_connection import some_db_function
 
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mylibrary',
-    version='0.1.2',
+    name='erplib',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[],  # List your package dependencies here
-    url='https://github.com/abhineshkr/mylibrary',
+    url='https://github.com/abhineshkr/erplib',
     author='Your Name',
     author_email='yourname@example.com',
     description='A simple example library',
