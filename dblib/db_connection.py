bbaaf86625b64c53f8eddb30ebc18a1ee9d33451
abhineshkr <abhineshkr1987@gmail.com>
2024-05-20 11:55:01 +0530

    db config

--- dblib/db_connection.py
+++ dblib/db_connection.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.pool import QueuePool
 from .config import settings
 
-print(settings.dict())
+#print(settings.)
 DATABASE_URL = f"postgresql://{settings.PRIMARY_POSTGRES_USER}:{settings.PRIMARY_POSTGRES_PASSWORD}@{settings.PRIMARY_POSTGRES_HOST}:{settings.PRIMARY_POSTGRES_PORT}/{settings.PRIMARY_POSTGRES_DB}"
 print(DATABASE_URL)
 
